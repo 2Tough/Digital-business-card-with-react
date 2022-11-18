9b0ba07d9d77e949cb1284c316150b78a70ea53a
2Tough <vainferro@gmail.com>
2022-11-18 22:52:44 +0300

    Added elements to remaining components

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 export default function Interests() {
     return (
-        <div>
+        <div className='container-interests'>
             <h2>Interests</h2>
+            <p>Gym, running, OAPU grinder, playing guitar.</p>
 
         </div>
     )
