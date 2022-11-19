38ec1ddd029e370a30681020f0bc24aaee218b94
2Tough <vainferro@gmail.com>
2022-11-19 13:34:11 +0300

    Final commit.

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import instagram from '../images/instagram.png'
+import github from '../images/github.png'
+import twitter from '../images/twitter.png'
+import facebook from '../images/facebook.png'
+
+
 
 export default function Interests() {
     return (
-        <div className='container-interests'>
-            <h2>Interests</h2>
-            <p>Gym, running, OAPU grinder, playing guitar.</p>
+        <div className="container-interests">
+            <div className="interests-box">
+                <h2>Interests</h2>
+                <p>Gym, running, OAPU grinder, playing guitar.</p>
+            </div>
+            <div className="images">
+                <img className="icons" src={ instagram }></img>
+                <img className="icons" src={ github }></img>
+                <img className="icons" src={ twitter }></img>
+                <img className="icons" src={ facebook }></img>
+            </div>
 
         </div>
     )
